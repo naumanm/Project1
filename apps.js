690cdb5a21457bedb3d6cbc964577bff7e5028ec
michael nauman <bodhiandmike@gmail.com>
2015-02-01 17:28:43 -0800

    pulling ISS objects in a loop

--- apps.js
+++ apps.js
@@ -1,19 +1,38 @@
 $(document).ready(function() {
   
   function initialize() {
+
+    // should define constructors
+    // should instintiate map and loop object
+
     // need to check local storage for last location
     var mapOptions = {
       center: { lat: 37.85 , lng: -122.6450},
       zoom: 10
     };
+
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    }
 
-  google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, 'load', initialize);
 
-  console.log("calling for ISS location data...");
-  $.getJSON("http://api.open-notify.org/iss-now.json", function (data) {
-    console.log("data from space", data);
-  });
+    setInterval(issPosition, 10000); 
+
+  }
+
+  function issPosition () {
+      $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
+        //var lat = data['iss_position']['latitude'];
+        //var lon = data['iss_position']['longitude'];
+
+        console.log(data);
+    });
+  }
+
+
+// ----------------------------------
+
+// lets start the party!
+
+initialize();
 
 });
