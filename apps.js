10b41cbc417556e26eb763adc78e4a13f9486a9d
michael nauman <bodhiandmike@gmail.com>
2015-02-04 13:30:17 -0800

    refactored local storage and helper functions

--- apps.js
+++ apps.js
@@ -2,7 +2,8 @@
 
 $(document).ready(function() {
   
-  // map object constructor
+// ---- constructors ----
+
   var Map = function mapObject () {
 
     var mapOptions = {
@@ -14,51 +15,19 @@ $(document).ready(function() {
     this.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   };
 
-  // get and return zoom from local storage
-  function getStoredZoom() {
-    if(parseInt(localStorage.getItem("userZoom")) > 0) {
-      $("#mapZoom").prop('value', localStorage.getItem("userZoom"));
-      return (parseInt(localStorage.getItem("userZoom")));
-    }
-  }
-
-  // get and return map style from local storage
-  function getStoredStyle () {
-    if(localStorage.getItem("mapStyle").length > 0) {
-      // update the UI to reflect the storage value
-      if (localStorage.getItem("mapStyle") === "satellite") {
-        $("#rad1").prop('checked', true);
-        $('#rad1').attr('checked', true);
-      } else if (localStorage.getItem("mapStyle") === "hybrid") {
-        $("#rad2").prop('checked', true);
-      } else if (localStorage.getItem("mapStyle") === "terrain") {
-        $("#rad3").prop('checked', true);
-      } else if (localStorage.getItem("mapStyle") === "roadmap") {
-        $("#rad4").prop('checked', true);
-      } else if (localStorage.getItem("mapStyle") === "undefined") {
-        console.log("mapStyle is undefined");
-      }
-      // return the storage item
-      return localStorage.getItem("mapStyle");
-    } else {
-        console.log("mapStyle length is < 0");
-        return "roadmap";
-    }
-  }
+// ---- main ----
 
   // initialize and start loop 
   function initialize() {
     var trackingArray = [];
     var myMap = new Map();
     attachEventListeners();
+    updateStyleUI();
+    updateZoomUI();
     setInterval(function () {issPositionLoop(myMap, trackingArray);}, 5000); 
   }
 
-  // attach listeners
-  function attachEventListeners() {
-    google.maps.event.addDomListener(window, 'load', initialize);
-  }
-
+  // execute on each setInterval
   function issPositionLoop (myMap, trackingArray) {
     // get ISS data and set callback
     $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
@@ -75,33 +44,15 @@ $(document).ready(function() {
     });
   }
   
-  function updateLocalStorage () {
-    // should wrap this in an if to see if a change is needed
-    localStorage.setItem("mapStyle", mapStyle());
-    localStorage.setItem("userZoom", userZoom());
-
-    console.log("Storage style: " + localStorage.getItem("mapStyle"));
-    console.log("Storage zoom: " + localStorage.getItem("userZoom"));
-  }
-
-  // updates UI for current map values
-  function updateContent (latLong) {
-    $(".latText").text(latLong.currentLat.toFixed(4));
-    $(".longText").text(latLong.currentLong.toFixed(4));
-    $(".zoomValue").text($("#mapZoom").val());
-    $(".altitudeText").text("431 km");
-    $(".velocityText").text("27,600 kph");
-  }
-
   // updates map, needs improvement
   function updateMap (myMap, trackingArray, latLong) {
 
     // update map object
     map = new google.maps.Map(document.getElementById('map-canvas'), {
       center: { lat: latLong.currentLat , lng: latLong.currentLong},
-      mapTypeId: mapStyle(),
+      mapTypeId: getMapStyleUI(),
       disableDefaultUI: true,
-      zoom: userZoom()
+      zoom: getUserZoomUI()
     });
 
     // put iss icon on center of map
@@ -127,8 +78,53 @@ $(document).ready(function() {
     } 
   }
 
+  // updates UI for current map values
+  function updateContent (latLong) {
+    $(".latText").text(latLong.currentLat.toFixed(4));
+    $(".longText").text(latLong.currentLong.toFixed(4));
+    $(".altitudeText").text("431 km");
+    $(".velocityText").text("27,600 kph");
+  }
+
+
+// ---- helper functions ----
+
+  function updateLocalStorage () {
+    // TODO: should wrap this in an if to see if a change is needed
+    localStorage.setItem("mapStyle", getMapStyleUI());
+    localStorage.setItem("userZoom", getUserZoomUI());
+  }
+
+  function attachEventListeners() {
+    google.maps.event.addDomListener(window, 'load', initialize);
+  }
+
+  // returns style from storage
+  function getStoredStyle () {
+    if(localStorage.getItem("mapStyle").length > 0) {
+      return localStorage.getItem("mapStyle");
+    } else {
+      return "roadmap";
+    }
+  }
+
+  // updates style UI from local storage
+  function  updateStyleUI () {
+    if (localStorage.getItem("mapStyle") === "satellite") {
+      $("#rad1").prop('checked', true);
+    } else if (localStorage.getItem("mapStyle") === "hybrid") {
+      $("#rad2").prop('checked', true);
+    } else if (localStorage.getItem("mapStyle") === "terrain") {
+      $("#rad3").prop('checked', true);
+    } else if (localStorage.getItem("mapStyle") === "roadmap") {
+      $("#rad4").prop('checked', true);
+    } else if (localStorage.getItem("mapStyle") === "undefined") {
+      console.log("mapStyle is undefined");
+    }
+  }
+
   // helper function to return the map style radio button value
-  function mapStyle() {
+  function getMapStyleUI() {
     if ($("#rad1")[0].checked) {
       return 'satellite';
     } else if ($("#rad2")[0].checked) {
@@ -140,9 +136,22 @@ $(document).ready(function() {
     } 
   }
 
-  // helper function to get value of zoom slider  
-  function userZoom() {
-    return parseInt($("#mapZoom").val(), 10);
+  // returns zoom value from storage
+  function getStoredZoom() {
+    if(parseInt(localStorage.getItem("userZoom")) > 0) {
+      $("#mapZoom").prop('value', localStorage.getItem("userZoom"));
+      return (parseInt(localStorage.getItem("userZoom")));
+    }
+  }
+
+  // updates zoom UI from local storage
+  function updateZoomUI () {
+    $("#mapZoom").prop('value', localStorage.getItem("userZoom") );
+  }
+
+  // returns value of zoom UI
+  function getUserZoomUI() {
+    return parseInt($("#mapZoom").prop('value'));
   }
 
 // ----------------------------------
